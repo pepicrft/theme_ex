c6b5f861696af91fb0de4fc23439afd5cb2943e3
Pedro <pedro@pepicrft.me>
2025-08-31 19:45:24 +0200

    Create project

---
+++ lib/theme_ex.ex
@@ -0,0 +1,18 @@
+defmodule ThemeEx do
+  @moduledoc """
+  Documentation for `ThemeEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThemeEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/theme_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule ThemeExTest do
+  use ExUnit.Case
+  doctest ThemeEx
+
+  test "greets the world" do
+    assert ThemeEx.hello() == :world
+  end
+end
